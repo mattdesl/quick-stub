ca5099e92fc56e0548462151773d87dbdbfa9584
mattdesl <dave.des@gmail.com>
2018-01-10 16:26:44 -0500

    new version with babel-core and babel-preset-env, default to using a directory

--- bin/budo.js
+++ bin/budo.js
@@ -1,42 +1,53 @@
 #!/usr/bin/env node
-var path = require('path')
+var path = require('path');
 var argv = require('minimist')(process.argv.slice(2), {
   alias: {
     auto: 'a',
     babel: 'b'
   }
-})
+});
 
-var entry = argv._[0] || 'index.js:bundle.js'
-var deps = ['budo']
-var opts = []
-opts.push('--live')
+var entry = argv._[0] || 'src/index.js:bundle.js';
+var deps = ['budo'];
+var opts = [];
+opts.push('--live');
 
-if (argv.babel || argv.auto) {
-  opts.push('--')
+if (argv.dir !== false) {
+  const dir = (typeof argv.dir === 'string' && argv.dir) ? argv.dir : 'app';
+  opts.push('--dir', dir);
+}
+
+if (argv.babel || argv.auto || argv.glslify) {
+  opts.push('--');
 }
 
 if (argv.babel) {
-  opts.push('-t', 'babelify')
-  deps.push('babelify', 'babel-preset-es2015')
+  opts.push('-t', 'babelify');
+  deps.push('babelify', 'babel-preset-env', 'babel-core');
 }
+
+if (argv.glslify) {
+  opts.push('-t', 'glslify');
+  deps.push('glslify');
+}
+
 if (argv.auto) {
-  opts.push('-t [ installify --save ]')
-  deps.push('installify')
+  opts.push('-t [ installify --save ]');
+  deps.push('installify');
 }
 
-opts = opts.join(' ')
+opts = opts.join(' ');
 
-var cmd = 'budo ' + entry + ' ' + opts
+var cmd = 'budo ' + entry + ' ' + opts;
 
-var template, output
+var template, output;
 if (argv.babel) {
-  var cwd = process.cwd()
-  template = path.resolve(__dirname, '..', 'templates', '.babelrc')
-  output = path.join(cwd, '.babelrc')
+  var cwd = process.cwd();
+  template = path.resolve(__dirname, '..', 'templates', '.babelrc');
+  output = path.join(cwd, '.babelrc');
 }
 
-console.log('adding "start" script to package.json:\n  ' + cmd)
+console.log('adding "start" script to package.json:\n  ' + cmd);
 require('../')({
   template: template,
   output: output,
@@ -46,4 +57,4 @@ require('../')({
       start: cmd
     }
   }
-})
+});
--- bin/build.js
+++ bin/build.js
@@ -1,17 +1,30 @@
 #!/usr/bin/env node
-var argv = require('minimist')(process.argv.slice(2))
+var argv = require('minimist')(process.argv.slice(2));
+var path = require('path');
 
-var deps = ['browserify', 'uglify-js']
-var entry = argv._[0] || 'index.js'
-var outfile = argv.o || argv.outfile || 'bundle.js'
-var cmd = "browserify "+entry+" | uglifyjs -m -c warnings=false > "+outfile
+var deps = ['browserify', 'uglify-js'];
+var entry = argv._[0] || 'src/index.js';
 
-console.log('adding "build" script to package.json:\n  '+cmd)
+let defaultOutfile = 'bundle.js';
+let defaultDir = 'app';
+if (argv.dir && typeof argv.dir === 'string') {
+  defaultDir = argv.dir;
+}
+
+if (argv.dir !== false) {
+  defaultOutfile = path.join(defaultDir, defaultOutfile);
+}
+
+var outfile = argv.o || argv.outfile || defaultOutfile;
+var cmd = 'browserify ' + entry + ' | uglifyjs -m -c warnings=false > ' + outfile;
+
+console.log('adding "bundle" script to package.json:\n  ' + cmd);
 require('../')({
   devDependencies: deps,
   packageTransform: {
     scripts: {
-      build: cmd
+      bundle: cmd
     }
   }
 })
+;
